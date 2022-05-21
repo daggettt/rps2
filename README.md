0ddae96871f298f2a25769416384bb9081894357
Rebecca <lorettadaggett.r@gmail.com>
2022-05-21 14:36:35 -0400

    Complete: Part 1 of Rock, Paper, Scissors Assignment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rps2
+This game is the first iteration of the project, and will be played entirely from the console.
+Part 2 of Rock, Paper, Scissors will be updated with a UI.
+
+This game has 5 rounds and is played against the CPU.
--- main.js
+++ main.js
@@ -1,56 +1,80 @@
-//Rock, Paper, Scissors
+//Rock, Paper, Scissors (in the Console)
 
-//1. player selects either rock, paper, or scissors
+let playerScore = 0;
+let cpuScore = 0;
 
-let playerChoice = "Rock"; 
-
-//2. computer selects either rock, paper, or scissors (randomly selected)
-function cpuChoice() {
+//1. Computer randomly returns Rock, Paper, or Scissors
+function cpuPlay() {
   let randomChoice = Math.floor(Math.random() * 3);
   //console.log(randomChoice);
 
   if (randomChoice == 0) {
-    return "Rock";
+    return "rock";
   } else if (randomChoice == 1) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissors";
+    return "scissors";
   }
 }
 
-//3. console prints "result" - win, lose, or tie
+//2. Takes Player Selection and Computer Selection and returns string w/ results
+function playRound(playerSelection, cpuSelection) {
 
-function result(playerChoice) {
-
-  let cpu = cpuChoice();
-
-  if (playerChoice == "Rock") {
-    if (cpu == "Paper") {
-      console.log("You played Rock. CPU played Paper. You lose!")
-    } else if (cpu == "Scissors") {
-      console.log("You played Rock. CPU played Scissors. You win!")
+  if (playerSelection == "rock") {
+    if (cpuSelection == "paper") {
+      cpuScore += 1;
+      return `You played Rock. CPU played Paper. You lose! The CPU has ${cpuScore} points.`;
+    } else if (cpuSelection == "scissors") {
+      playerScore += 1;
+      return `You played Rock. CPU played Scissors. You win! You have ${playerScore} points.`;
     } else {
-      console.log("You played Rock. CPU played Rock, too. It's a tie!")
+      return `You played Rock. CPU played Rock, too. It's a tie! The score is ${playerScore} : ${cpuScore}.`;
     };
 
-  } else if (playerChoice == "Paper") {
-    if (cpu == "Scissors") {
-      console.log("You played Paper. CPU played Scissors. You lose!")
-    } else if (cpu == "Rock") {
-      console.log("You played Paper. CPU played Rock. You win!")
+  } else if (playerSelection == "paper") {
+    if (cpuSelection == "scissors") {
+      cpuScore += 1;
+      return `You played Paper. CPU played Scissors. You lose! The CPU has ${cpuScore} points.`;
+    } else if (cpuSelection == "rock") {
+      playerScore += 1;
+      return `You played Paper. CPU played Rock. You win! You have ${playerScore} points.`;
     } else {
-      console.log("You played Paper. CPU played Paper, too. It's a tie!")
+      return `You played Paper. CPU played Paper, too. It's a tie! The score is ${playerScore} : ${cpuScore}.`;
     };
 
-  } else if (playerChoice == "Scissors") {
-    if (cpu == "Rock") {
-      console.log("You played Scissors. CPU played Rock. You lose!")
-    } else if (cpu == "Paper") {
-      console.log("You played Scissors. CPU played Paper. You win!")
+  } else if (playerSelection == "scissors") {
+    if (cpuSelection == "rock") {
+      cpuScore += 1;
+      return `You played Scissors. CPU played Rock. You lose! The CPU has ${cpuScore} points.`;
+    } else if (cpuSelection == "paper") {
+      playerScore += 1;
+      return `You played Scissors. CPU played Paper. You win! You have ${playerScore} points.`;
     } else {
-      console.log("You played Scissors. CPU played Scissors, too. It's a tie!")
+      return `You played Scissors. CPU played Scissors, too. It's a tie! The score is ${playerScore} : ${cpuScore}.`;
     };
   };
 };
 
-result("Scissors");
+//3. Calls Play Round 5 times, and announces a winner at the end
+function game() {
+
+while (playerScore < 5 || cpuScore < 5) {
+  let playerSelection = prompt("Please select Rock, Paper, or Scissors");
+  playerSelection = playerSelection.toLowerCase();
+  
+  let cpuSelection = cpuPlay();
+  cpuSelection = cpuSelection.toLowerCase();
+
+  
+  alert(playRound(playerSelection, cpuSelection), console.log(playerScore + " : " + cpuScore));
+
+  if (playerScore == 5) {
+    return alert("Congrats! You won!");
+  } else if (cpuScore == 5){
+    return alert("Sorry! You lost!");
+  };
+  
+};
+}
+
+game();
